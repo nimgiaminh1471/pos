44e5966196661650338cc17cad5591cd1bf7f9cc
minh.nim <nimgiaminh1471@gmail.com>
2025-07-19 08:52:48 +0700

    Change size

--- resources/views/vendor/invoices/templates/default.blade.php
+++ resources/views/vendor/invoices/templates/default.blade.php
@@ -144,8 +144,8 @@
             );  
             $b64image = base64_encode(file_get_contents('https://img.vietqr.io/image/VCB-9777729933-qr_only.png?amount='. $invoice->total_amount . '&addInfo=CK&accountName=VUONG%20MINH%20QUAN', false, stream_context_create($arrContextOptions)))
         @endphp
-        <div style="position: absolute; top: 190px; right: 0px; text-align: center">
-            <img src="data:image/png;base64,{{ $b64image }}" alt="" height="150">
+        <div style="position: absolute; top: 200px; right: 0px; text-align: center">
+            <img src="data:image/png;base64,{{ $b64image }}" alt="" height="200">
             <p style="font-size: 12px">Scan để thanh toán</p>
         </div>
 
