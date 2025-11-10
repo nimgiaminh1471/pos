95e64a81dad8b06749522c4b2956ad24b0caeaa5
minh.nim <87536179+nimgiaminh1471@users.noreply.github.com>
2025-11-10 14:47:01 +0700

    update logic

--- resources/views/livewire/product-crud.blade.php
+++ resources/views/livewire/product-crud.blade.php
@@ -39,7 +39,7 @@
                         <td class="border px-4 py-2">{{ $product->name }}</td>
                         <td class="border px-4 py-2"><image width="100" src="{{ asset('storage/' .$product->image) }}"></td>
                         <td class="border px-4 py-2">{{ number_format($product->price, 0, ',', '.') }}</td>
-                        <td class="border px-4 py-2">{{ $product->revenue_type == 'normal' ? 'Bình thường' : 'Đặc biệt' }}</td>
+                        <td class="border px-4 py-2">{{ $product->revenue_type == 'special' ? 'Đặc biệt' : 'Bình Thường' }}</td>
                         <td class="border px-4 py-2">
                             <button wire:click="edit({{ $product->id }})"
                                 class="flex px-4 py-2 bg-gray-500 text-gray-900 cursor-pointer">Chỉnh sửa</button>
